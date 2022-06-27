e43417b0aa84df4444472e9ee3c13266cda95295
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-06-27 12:18:00 +0900

    MainActivity.kt 버튼 구현 / 사용할 프래그먼트 2개 생성

--- app/src/main/java/com/duran/fragment_ex/MainActivity.kt
+++ app/src/main/java/com/duran/fragment_ex/MainActivity.kt
@@ -2,10 +2,15 @@ package com.duran.fragment_ex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_main = findViewById<Button>(R.id.button1)
+        val btn_menu = findViewById<Button>(R.id.button2)
+
     }
 }
